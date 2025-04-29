2d4fb0e98fe201f480b72b229fc914384d78ba7a
Nikita Upray <129362548+Nikita-Upray@users.noreply.github.com>
2025-04-29 10:19:54 +0530

    Update Ad-Hoc Analysis SQL Query.sql

--- Ad-Hoc Analysis SQL Query.sql
+++ Ad-Hoc Analysis SQL Query.sql
@@ -11,11 +11,8 @@ WHERE
 AND
 	region = "APAC";
 
-/* 2. What is the percentage of unique product increase in 2021 vs. 2020? The 
-final output contains these fields, 
-unique_products_2020 
-unique_products_2021 
-percentage_chg */
+/* 2. What is the percentage of unique product increase in 2021 vs. 2020? 
+	The final output contains these fields, unique_products_2020, unique_products_2021, percentage_chg */
 
 
 WITH unique_counts AS 
@@ -35,10 +32,8 @@ FROM
 	unique_counts;
 
 
-/*  3. Provide a report with all the unique product counts for each  segment  and 
-sort them in descending order of product counts. The final output contains 2 fields, 
-segment 
-product_count */
+/*  3. Provide a report with all the unique product counts for each  segment  and sort them in descending order of product counts.
+	The final output contains 2 fields, segment, product_count */
 
 
 SELECT
@@ -52,12 +47,8 @@ ORDER BY
 	product_count DESC;
 
 
-/* 4. Follow-up: Which segment had the most increase in unique products in 
-2021 vs 2020? The final output contains these fields, 
-segment 
-product_count_2020 
-product_count_2021 
-difference */
+/* 4. Follow-up: Which segment had the most increase in unique products in 2021 vs 2020? 
+	The final output contains these fields, segment, product_count_2020, product_count_2021, difference */
 
 WITH segment_counts AS 
 (
@@ -88,10 +79,7 @@ ORDER BY
     
     
 /* 5. Get the products that have the highest and lowest manufacturing costs. 
-The final output should contain these fields, 
-product_code 
-product 
-manufacturing_cost  */
+	The final output should contain these fields, product_code, product, manufacturing_cost  */
 
 WITH ranked_costs AS 
 (
@@ -122,12 +110,9 @@ OR
     
     
     
-/* 6. Generate a report which contains the top 5 customers who received an 
-average high  pre_invoice_discount_pct  for the  fiscal  year 2021  and in the 
-Indian  market. The final output contains these fields, 
-customer_code 
-customer 
-average_discount_percentage */
+/* 6. Generate a report that contains the top 5 customers who received an average high  pre_invoice_discount_pct  for the  fiscal  year 2021, 
+and in the Indian  market. 
+	The final output contains these fields, customer_code, customer, average_discount_percentage */
 
 SELECT
 	id.customer_code,
@@ -152,13 +137,9 @@ LIMIT 5;
 
 
 
-/* 7. Get the complete report of the Gross sales amount for the customer  “Atliq 
-Exclusive”  for each month  .  This analysis helps to  get an idea of low and 
+/* 7. Get the complete report of the Gross sales amount for the customer  “Atliq Exclusive”  for each month  .  This analysis helps to  get an idea of low and 
 high-performing months and take strategic decisions. 
-The final report contains these columns: 
-Month 
-Year 
-Gross sales Amount */
+	The final report contains these columns: Month, Year, Gross sales Amount */
 
 SELECT 
     MONTH(sm.date) AS Month,
@@ -187,10 +168,8 @@ ORDER BY
 
 
 
-/* 8. In which quarter of 2020, got the maximum total_sold_quantity? The final 
-output contains these fields sorted by the total_sold_quantity, 
-Quarter 
-total_sold_quantity */
+/* 8. In which quarter of 2020, got the maximum total_sold_quantity? 
+	The final output contains these fields sorted by the total_sold_quantity, Quarter, total_sold_quantity */
 
 SELECT
 	CASE
@@ -210,11 +189,8 @@ GROUP BY
 
 
 
-/* 9. Which channel helped to bring more gross sales in the fiscal year 2021 
-and the percentage of contribution?  The final output contains these fields, 
-channel 
-gross_sales_mln 
-percentage */
+/* 9. Which channel helped to bring more gross sales in the fiscal year 2021 and the percentage of contribution? 
+	The final output contains these fields, channel, gross_sales_mln, percentage */
 
 
 WITH channel_sales AS
@@ -252,13 +228,8 @@ ORDER BY
 
 
 
-/* 10. Get the Top 3 products in each division that have a high 
-total_sold_quantity in the fiscal_year 2021? The final output contains these fields, 
-division 
-product_code 
-product 
-total_sold_quantity 
-rank_order */
+/* 10. Get the Top 3 products in each division that have a high total_sold_quantity in the fiscal_year 2021? 
+	The final output contains these fields, division, product_code, product, total_sold_quantity, rank_order */
 
 WITH product_sales AS
 (
